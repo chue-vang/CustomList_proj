41816699f568d6a8e81ae6779ac3452b163b7d01
unknown <chue.vang0@gmail.com>
2021-01-04 10:56:34 -0600

    Initial Commit

---
+++ CustomListTest/ListTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void Add_StringToList_AddingOneString()
+        {
+            //Arrange
+            CustomList<string> testList = new CustomList<string>();
+            string testString = "Testing string";
+
+            //Act
+            testList.Add(testString);
+
+            //Assert
+            Assert.AreEqual(testString, testList[0]);
+        }
+    }
+
+    [TestMethod]
+    public void Add_IntToList_AddOneInt()
+    {
+        //Arrange
+        CustomList<int> testList = new CustomList<int>;
+        int number = 3;
+        int expected = 3;
+
+        //Act
+        testList.Add(number);
+
+        //Assert
+        Assert.AreEqual(expected, number[0]);
+    }
+
+    [TestMethod]
+    public void Add_MultipleIntToList_AddThreeInt()
+    {
+        //Arange
+        CustomList<int> testList = new CustomList<int>() { 1, 2, 3 };
+        int expected = 3;
+
+        //Act
+
+
+        //Assert
+        Assert.AreEqual(expected, testList[2]);
+    }
+}
---
+++ CustomListTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5824017d-43b7-417e-8ee5-f8031467c9f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
