29ab069c95520f1d6b5c19949f617cb09258347d
unknown <chue.vang0@gmail.com>
2021-01-05 09:51:45 -0600

    Completed five Add Unit Test

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,6 +8,49 @@ namespace CustomList
 {
     public class CustomList<T>
     {
+        //member variables
+        T[] items = new T[4];
+        //needs to be read only
+        public int Count
+        {
+            get;
+            private set;
+        }
+        //count and capacity needs to be properties
+        public int Capacity
+        {
+            get;
+            set;
+        }
+
+
+
+
+        //constructor
+        public CustomList()
+        {
+            Capacity = 0;
+            Count = 0;
+        }
+
+
+
+
+        //member methods
+        //1. add item to next available index
+        //2. increment count, assuming item was added successfuly        
+        //3. if count and capacity are the same, then create more space
+
+        public void Add(T item)
+        {
+            if (Count == Capacity)
+            {
+                
+            }
+            items[Count] = item;
+            Count++;
+        }
+
 
     }
 }
--- CustomListTest/ListTest.cs
+++ CustomListTest/ListTest.cs
@@ -8,16 +8,17 @@ namespace CustomListTest
     public class ListTest
     {
         [TestMethod]
-        public void Add_AddIntToList_AddOneInt()
+        public void Add_AddIntToList_CheckZeroIndex()
         {
             //Arange
             CustomList<int> testList = new CustomList<int>();
-            int testNumber = 9;
-            int expected = 9;
+            int testNumber = 1;
+            int expected = 1;
             int actual;
 
             //Act
-            actual = testList.Add(testNumber);
+            testList.Add(testNumber);
+            actual = testList[0];
 
             //Assert
             Assert.AreEqual(expected, actual);
@@ -33,14 +34,15 @@ namespace CustomListTest
             string actual;
 
             //Act
-            actual = testList.Add(testString);
+            testList.Add(testString);
+            actual = testList[0];
 
             //Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Add_AddItemsToList_DoubleCapacityOfArray()
+        public void Add_AddItemsToList_CapacityOfArray()
         {
             //Arrange
             CustomList<int> testList = new CustomList<int>();
@@ -61,15 +63,56 @@ namespace CustomListTest
         [TestMethod]
         public void Add_MultipleItemsToList_CountIncreasesByOne()
         {
+            //Arange
             CustomList<int> testList = new CustomList<int>();
             int expected = 2;
             int actual;
 
+            //Act
             testList.Add(1);
             testList.Add(2);
             actual = testList.Count;
 
+            //Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        //after capacity is increased, make sure one of the previous indexes is still the same
+        [TestMethod]
+        public void Add_AddItemsToFullArray_CheckingToMakeSureItemsCopiedOver()
+        {
+            //Arange
+            CustomList<int> testList = new CustomList<int>();
+            int expected0 = 1;
+            int expected1 = 2;
+            int expected2 = 3;
+            int expected3 = 4;
+            int expected4 = 5;
+            int actual0;
+            int actual1;
+            int actual2;
+            int actual3;
+            int actual4;
+
+
+            //Act
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            actual0 = testList[0];
+            actual1 = testList[1];
+            actual2 = testList[2];
+            actual3 = testList[3];
+            actual4 = testList[4];
+
+            //Assert
+            Assert.AreEqual(expected0, actual0);
+            Assert.AreEqual(expected1, actual1);
+            Assert.AreEqual(expected2, actual2);
+            Assert.AreEqual(expected3, actual3);
+            Assert.AreEqual(expected4, actual4);
         }
     }
 }
